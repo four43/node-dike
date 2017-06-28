d30133a3e3ac10f4413ac02e7c34add588f772a9
Seth Miller <smiller@aerisweather.com>
2017-06-28 11:40:59 -0500

    Spell stream correctly, GOSH

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import enhance from "./Stream/enhance";
 import Transform from "./Stream/Transform";
 
-interface IEnhancedWritableSteam extends NodeJS.WritableStream, Promise<undefined> {
+interface IEnhancedWritableStream extends NodeJS.WritableStream, Promise<undefined> {
 }
-interface IEnhancedReadableSteam extends NodeJS.ReadableStream, Promise<undefined> {
+interface IEnhancedReadableStream extends NodeJS.ReadableStream, Promise<undefined> {
 }
-interface IEnhancedTransformSteam extends NodeJS.ReadableStream, NodeJS.WritableStream, Promise<undefined> {
+interface IEnhancedTransformStream extends NodeJS.ReadableStream, NodeJS.WritableStream, Promise<undefined> {
 }
 
 export {
 	enhance,
 	Transform,
 	
-	IEnhancedWritableSteam,
-	IEnhancedReadableSteam,
-	IEnhancedTransformSteam
+	IEnhancedWritableStream,
+	IEnhancedReadableStream,
+	IEnhancedTransformStream
 };
